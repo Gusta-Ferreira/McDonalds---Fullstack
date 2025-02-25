f8c88db3aa436c99b7c431d638c2368865dbace7
Gusta-Ferreira <gustavoferreirag007@gmail.com>
2025-02-25 01:04:36 -0300

    feat: ensure cart total is shown

--- src/app/[slug]/menu/components/cart-seet.tsx
+++ src/app/[slug]/menu/components/cart-seet.tsx
@@ -1,31 +1,45 @@
+
 import { useContext } from "react";
 
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
+import { formatCurrency } from "@/helpers/format-currency";
 
 import { CartContext } from "../contexts/cart";
 import CardProductItem from "./cart-product-item";
 
 
 const CartSheet = () => {
-  const { isOpen, toggleCart, products } = useContext(CartContext);
+  const { isOpen, toggleCart, products, total } = useContext(CartContext);
   return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
       <SheetContent className="w-[80%]">
         <SheetHeader>
           <SheetTitle className="text-left">Sacola</SheetTitle>
-          <SheetDescription>
-          </SheetDescription>
         </SheetHeader>
-        <div className="py-5">
+        <div className=" flex flex-col h-full py-5">
+        <div className="flex-auto">
         {products.map((product) => (
           <CardProductItem key={product.id} product={product}/>
         ))}
+        </div>
+        <Card className="mb-6">
+            <CardContent className="p-5">
+              <div className="flex justify-between">
+                <p className="text-sm text-muted-foreground">Total</p>
+                <p className="text-sm font-semibold">{formatCurrency(total)}</p>
+              </div>
+            </CardContent>
+          </Card>
+        <Button className="w-full rounded-full">
+          Finalizar Pedido
+        </Button>
         </div>
       </SheetContent>
     </Sheet>
